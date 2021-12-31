8b5eb19bc5d817b4dd14f0e9b1131dc1dcd5ee5f
VuribindiTeja <96712155+VuribindiTeja@users.noreply.github.com>
2021-12-31 16:46:06 +0530

    upload

---
+++ src/components/AndhraBank.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AndhraBank extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AndhraBank { this.props.location}</h1>
+            </div>
+        )
+    }
+}
